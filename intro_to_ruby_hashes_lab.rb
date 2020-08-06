3f5b421db6bee8f50ee87dac36ebf4723036f8c1
Brian Donnoe <dunow33@hotmail.com>
2020-08-06 23:43:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,10 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads=>
-	pieces}}
+	monopoly = {:railroads =>
+	              {
+	                :pieces => 4,
+	                :rent}}
 
 end
 
