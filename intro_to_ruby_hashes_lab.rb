6f237e2074f4a5aba88deed0aa67aa3c60af8646
Brian Donnoe <dunow33@hotmail.com>
2020-08-06 23:58:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -46,6 +46,7 @@ def monopoly_with_third_tier
 	                
 	                    
 	                  },
+	                },
 	                :rent_in_dollars => 
 	                {
 	                  :one_piece_owned => 25,
